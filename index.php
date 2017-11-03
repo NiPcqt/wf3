8f72495332a3ca6787f1616ad9301157f28a6367
Nicolas Pocquet <nicolas.pocquet@outlook.fr>
2017-11-03 12:43:12 +0100

    Aprés modifications
    ajout nom, prénom et age

--- index.php
+++ index.php
@@ -6,7 +6,27 @@
 <body>
 
   <?php
-    echo "Bonjour, je suis un script PHP ! Super hein ?";
+    $nom= "Bertrand";
+    $prenom= "Adélaïde";
+    
+
+    echo "Bonjour,".$prenom ." ".$nom;
+    
+    echo "<br />";
+    $age= "20";
+
+    echo "J'ai $age ans";
+    
+    echo "<br />";
+
+    if ($age>18 && $prenom == "Adélaïde") {
+        echo "Majeur";
+    }elseif ($age >= 15) {
+        echo "Ado";
+    }else {
+        echo "Gamin !";
+    }
+
   ?>
 
 </body>
